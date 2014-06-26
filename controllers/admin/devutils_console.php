e2d6860209477dce37ae43be1d3d5a8542ab534f
vanilla thunder <root@VM-MB-DEV1.heinergroup.com>
2014-06-26 18:22:47 +0200

    up

--- controllers/admin/devutils_console.php
+++ controllers/admin/devutils_console.php
@@ -1,57 +1,56 @@
 <?php
 
-    /** The MIT License (MIT)
-     *  vt Dev Utils
-     *  Copyright (C) 2014  Marat Bedoev
-     *
-     * Permission is hereby granted, free of charge, to any person obtaining a copy
-     * of this software and associated documentation files (the "Software"), to deal
-     * in the Software without restriction, including without limitation the rights
-     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     * copies of the Software, and to permit persons to whom the Software is
-     * furnished to do so, subject to the following conditions:
-     */
+/** The MIT License (MIT)
+ *  vt Dev Utils
+ *  Copyright (C) 2014  Marat Bedoev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ */
+class devutils_console extends oxAdminView
+{
 
-	class devutils_console extends oxAdminView
-	{
+    protected $_tpl = 'vt_devutils_console.tpl';
 
-		protected $_tpl = 'vt_devutils_console.tpl';
+    public function init()
+    {
+        parent::init();
 
-		public function init()
-		{
-			parent::init();
-			$GLOBALS["me"] = $user = oxRegistry::getConfig()->getUser();
-			$GLOBALS["session"] = $user->getSession();
-			$GLOBALS["order"] = array_shift($user->getOrders(1)->getArray());
-			$GLOBALS["basket"] = $user->getSession()->getBasket();
-		}
+    }
 
-		public function render()
-		{
-			parent::render();
+    public function render()
+    {
+        parent::render();
 
-			return $this->_tpl;
-		}
+        return $this->_tpl;
+    }
 
-		public function doTest()
-		{
-			global $me,  $session, $order, $basket;
+    public function doTest()
+    {
+        $cfg = oxRegistry::get("oxConfig");
+        $code = $cfg->getParameter("codeinput");
+        $this->_aViewData["codeinput"] = $code;
 
-			$cfg = oxRegistry::get("oxConfig");
-			$code = $cfg->getParameter("codeinput");
-			$this->_aViewData["codeinput"] = $code;
+        $me = $user = oxRegistry::getConfig()->getUser();
+        $session = $user->getSession();
+        $order = array_shift($user->getOrders(1)->getArray());
+        $basket = $user->getSession()->getBasket();
 
-			ob_start();
+        $c = (class_exists("blCopernicaApi")) ? oxRegistry::get("blCopernicaApi") : false;
+
+        $fnc = function($code) use ($cfg, $me,$session,$order,$basket,$c) {
+            return eval($code);
+        };
+
+        ob_start();
+        $this->addTplParam("codeerror", $fnc($code));
+        $this->addTplParam("codeoutput",ob_get_clean());
 
 
-			//echo "$code";
-			$ex = eval($code);
+    }
 
-			//$encoder = oxRegistry::get("oxSeoEncoder");
-
-			$this->_aViewData["codeoutput"] = ob_get_contents();
-			$this->_aViewData["codeerror"] = $ex;
-			ob_end_clean();
-		}
-
-	}
+}
